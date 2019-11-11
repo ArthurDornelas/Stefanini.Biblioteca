7e87e59a1eac844d8095598106f9c447928d1c20
Arthur Dornelas <arthurdornelas99@gmail.com>
2019-11-11 19:41:49 -0300

    Update Program.cs

--- Biblioteca/Biblioteca/Program.cs
+++ Biblioteca/Biblioteca/Program.cs
@@ -18,10 +18,10 @@ namespace Biblioteca
             int cod = -1;
             while (cod != 0)
             {
-                Console.WriteLine("Digite o codigo do Menu");
-                Console.WriteLine("Digite 1 para adicionar um livro");
-                Console.WriteLine("Digite 2 para listar livros");
-                
+                Console.WriteLine("-------------MENU---------------");
+                Console.WriteLine("Digite 1 Para Adicionar Um Livro");
+                Console.WriteLine("Digite 2 Para Listar Livros");
+                Console.WriteLine("Digite 0 Para Sair");
 
                 cod = Convert.ToInt32(Console.ReadLine());
                 switch (cod)
@@ -38,28 +38,28 @@ namespace Biblioteca
 
         private static void AddLivro()
         {
-            Console.WriteLine("Digite o numero da estante do livro");
+            Console.WriteLine("Digite o Numero da Estante do Livro");
             int NumeroEst = Convert.ToInt32(Console.ReadLine());
             Estante Est = new Estante();
             Est.Numero = NumeroEst;
             Biblioteca.Estantes.Add(Est);
 
-            Console.WriteLine("Digite o numero da pratileira do livro");
+            Console.WriteLine("Digite o Numero da Pratileira do Livro");
             int NumeroPrat = Convert.ToInt32(Console.ReadLine());
             Prateleira Prat = new Prateleira();
             Prat.Numero = NumeroPrat;
             Est.Prateleiras.Add(Prat);
 
-            Console.WriteLine("Digite o nome do livro");
+            Console.WriteLine("Digite o Nome do Livro");
             Livro Book = new Livro();
             Book.Titulo = Console.ReadLine();
 
-            Console.WriteLine("Digite a categoria do livro");
+            Console.WriteLine("Digite a Categoria do Livro");
             CategoriaLivro Cat = new CategoriaLivro();
             Cat.Nome = Console.ReadLine();
             Book.Categoria = Cat;
 
-            Console.WriteLine("Digite o autor do livro");
+            Console.WriteLine("Digite o Autor do Livro");
             AutorLivro Aut = new AutorLivro();
             Aut.Nome = Console.ReadLine();
             Book.Autor = Aut;
